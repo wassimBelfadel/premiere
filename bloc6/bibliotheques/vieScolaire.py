d4f60898daee9b2c55cbaf2edf5798b84f2d8bac
seynave <r.seynave59@gmail.com>
2020-05-21 03:12:34 +0200

    fin sequence bibliotheque

---
+++ bloc6/bibliotheques/vieScolaire.py
@@ -0,0 +1,7 @@
+def moyenne(liste):
+    """
+    Description : calcule la moyenne
+    liste (list) : liste de notes(float)
+    return (float) : moyenne d'un éléve
+    """
+    return sum(liste)/len(liste)
