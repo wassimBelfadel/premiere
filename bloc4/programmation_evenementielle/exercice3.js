a5b97a74bc52b45c1ed71848ff245e5961ca256d
seynave <r.seynave59@gmail.com>
2020-06-05 00:44:41 +0200

    fini

--- bloc4/programmation_evenementielle/exercice3.js
+++
@@ -1,6 +0,0 @@
-function popUpUn(){
-  window.alert('un événement clic sur UN a déclenché cette fenêtre')
-}
-function popUpDeux(){
-  window.alert('un événement clic sur DEUX a déclenché cette fenêtre')
-}
