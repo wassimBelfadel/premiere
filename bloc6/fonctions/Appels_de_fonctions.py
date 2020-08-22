e786b8e490cc990ec5a2f370eda4f926c538dc4f
seynave <r.seynave59@gmail.com>
2020-08-22 17:30:04 +0200

    rentree

--- bloc6/fonctions/Appels_de_fonctions.py
+++
@@ -1,4 +0,0 @@
-# Laissez la ligne de code ci-dessous : elle est nécessaire (On verra précissemment ce que c'est plus tard).
-from ExemplesFonctions import *
-
-# Tappez des lignes de codes ci-dessous pour APPELER les fonctions dis_bonjour, racine,tirage_de_6_faces, tirage_de_10_faces, est_majeur
