71dc8fa737efc88d77ae37db188c1cd0099c2348
seynave <r.seynave59@gmail.com>
2020-11-16 18:39:26 +0100

    reorganisation

--- bloc6/bibliotheques/vieScolaire.py
+++
@@ -1,7 +0,0 @@
-def moyenne(liste):
-    """
-    Description : calcule la moyenne
-    liste (list) : liste de notes(float)
-    return (float) : moyenne d'un éléve
-    """
-    return sum(liste)/len(liste)
