9086785c245126c0d9888be35250e665a025ec38
wassimBelfadel <70703688+wassimBelfadel@users.noreply.github.com>
2021-02-17 22:06:01 +0100

    Add files via upload

---
+++ Boucle while Exercice 8.py
@@ -0,0 +1,17 @@
+def afficheCarre(x,y):
+    a = 0
+    b = 0
+    while a < x:
+        a = a + 1
+        while b < y:
+            b = b + 1
+            print("*",'#'*y,"*")
+
+def afficheCarre2(k):
+    a = 0
+    b = 0
+    while a < k:
+        a = a + 1
+        while b < k:
+            b = b + 1
+            print("*",'#'*k,"*")
