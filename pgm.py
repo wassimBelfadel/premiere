27c9000374f9548f6ff48b11f6fa32482823845a
wassimBelfadel <70703688+wassimBelfadel@users.noreply.github.com>
2021-03-30 09:52:44 +0200

    Add files via upload

---
+++ pgm.py
@@ -0,0 +1,28 @@
+###Ex3
+##t = []
+##for i in range(0,9,2):
+##    t.append(i**2)
+##
+###Ex4
+##t = [0]*10
+##for i in range(len(t)):
+##    t[i] = 10 - 1
+
+#Ex5
+##def longueurNomV1(liste_nom):
+##    liste_longueur =[]
+##    for nom in liste_nom:
+##       liste_longueur.append(len(nom))
+##    print(liste_longueur)
+##    
+##def longueurNomV2(liste_nom):
+##    liste_longueur = [0]*len(liste_nom)
+##    for i in range(len(liste_nom)):
+##        liste_nom[i] = len(liste_nom[i])
+##    print(liste_nom)
+
+#Ex6
+def afficheListe(t):
+    (['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre',end = "/n"])
+    print(t)
+    
